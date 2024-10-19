1f40ae2e8bb09cd9ee37cbd2dfd821ddc32b15a6
abelgacem <amarbelgacem@hotmail.fr>
2024-10-19 08:55:40 +0000

    upd

---
+++ docs/assets/js/todo.table_tab_select.v01.js
@@ -0,0 +1,20 @@
+// V01
+// Handle link clicks to activate tabs
+document.querySelectorAll('.tab-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+      e.preventDefault(); // Prevent default link behavior
+      const target = this.getAttribute('data-target'); // Get the target tab ID
+
+      // Show the target tab
+      const tab = new bootstrap.Tab(document.querySelector(`${target}-tab`));
+      tab.show();
+
+      // Update the active class for tabs
+      document.querySelectorAll('.nav-link').forEach(navLink => {
+          navLink.classList.remove('active');
+      });
+      document.querySelector(`${target}-tab`).classList.add('active');
+  });
+});
+
+{/* <a href="#" class="tab-link" data-target="#readme">Readme</a> */}
