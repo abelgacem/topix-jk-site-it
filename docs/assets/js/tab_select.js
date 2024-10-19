d55150ebdcc99675ca7a351fff10d2343fe4c59e
abelgacem <amarbelgacem@hotmail.fr>
2024-10-19 20:27:31 +0000

    upd

--- docs/assets/js/tab_select.js
+++ docs/assets/js/tab_select.js
@@ -1,31 +1,41 @@
 //v01
 // document.addEventListener("DOMContentLoaded", function() {
-document.addEventListener("hashchange", function() {
+document.addEventListener("DOMContentLoaded", function() {
   
   // Get the current URL fragment (if any)
-  var hash = window.location.hash;
-  console.log('hash = ', hash)
+  var hashTabContent  = window.location.hash;
+  var hashTabNav      = hashTabContent + "-tab"; 
+  console.log('hashTabContent  = ', hashTabContent)
+  console.log('hashTabNav      = ', hashTabNav)
 
   // Check if the hash is set and corresponds to a tab id
-  if (hash) {
-    var targetTab = document.querySelector(hash);
-    var activeTab = document.querySelector('.tab-pane.active');
-    console.log('targetTab = ', targetTab)
-    console.log('activeTab = ', activeTab)
-    if (targetTab) {
+  if (hashTabContent) {
+    var targetContent = document.querySelector(hashTabContent);
+    var targetNav     = document.querySelector(hashTabNav);
+    var activeContent = document.querySelector('.tab-pane.active');
+    var activeNav     = document.querySelector('.nav-link.active');
+    console.log('targetContent = ', targetContent)
+    console.log('targetNav     = ', targetNav)
+    console.log('activeContent = ', activeContent)
+    console.log('activeNav     = ', activeNav)
+    if (targetContent) {
       
-      // show target tab
-      targetTab.classList.add('show');
+      // Deactivate all Tab
+      document.querySelectorAll('.nav-link.active, .tab-pane.show').forEach(function(el) {
+        el.classList.remove('active', 'show');
+      });
 
-      // Deactivate previous active tab
-      document.querySelector('.tab-pane.active').classList.remove('show', 'active');      
+      // Active target
+      targetNav.classList.add('active');
+      targetContent.classList.add('show');
+      targetContent.classList.add('active');
 
-      // Activate the target tab
-      targetTab.classList.add('active');
-
+      
       // Remove the hash from the URL
-      history.replaceState(null, '', window.location.pathname + window.location.search);
-
+      // history.replaceState(null, '', window.location.pathname + window.location.search);
+      
     }
   }
 });
+// if (!hash) return; // If no hash, do nothing
+// document.querySelector('.tab-pane.active').classList.remove('show', 'active');      
--- docs/categories/_tools/list/docker/list/ref.md
+++ docs/categories/_tools/list/docker/list/ref.md
@@ -4,40 +4,43 @@ mx:
   ref:
 ---
 
+
 # Dockercompose
+
+
 |Context|Tag|Url|
 |-|-|-|
-|intro|-|https://docs.docker.com/compose/|
-|file, specification|-|https://docs.docker.com/compose/compose-file/|
-<br>
+|intro||https://docs.docker.com/compose/|
+|file, specification||https://docs.docker.com/compose/compose-file/|
+
 
 # Dockerfile
+
 |Context|Tag|Url|
 |-|-|-|
 |intro|-|https://docs.docker.com/engine/reference/builder/|
-<br>
+
 
 # Network
+
 |Context|Tag|Url|
 |-|-|-|
 |-|-|https://docs.docker.com/compose/networking/|
-<br>
+
 
 # Dind
+
 |Context|Tag|Url|
 |-|-|-|
 |doc||https://devopscube.com/run-docker-in-docker/
-<br>
 
-# Variable
-|Context|Tag|Url|
-|-|-|-|
-<br>
+
 
 # Other
+
 |Context|Tag|Url|
 |-|-|-|
 |Manifest|-|https://docs.docker.com/engine/reference/commandline/manifest/|
 ||-|- https://blog.eleven-labs.com/fr/monitorer-ses-containers-docker/|
-<br>
+
 
